8e41abca067a7fe6d697b722e1aea71a439d6954
Li Jin <dragon-fly@qq.com>
2017-03-31 22:45:04 +0800

    update box2d.

--- Source/3rdParty/Box2D/Dynamics/Joints/b2WheelJoint.h
+++ Source/3rdParty/Box2D/Dynamics/Joints/b2WheelJoint.h
@@ -72,15 +72,12 @@ struct b2WheelJointDef : public b2JointDef
 };
 
 /// A wheel joint. This joint provides two degrees of freedom: translation
-/// along an axis fixed in bodyA and rotation in the plane. You can use a
-/// joint limit to restrict the range of motion and a joint motor to drive
-/// the rotation or to model rotational friction.
+/// along an axis fixed in bodyA and rotation in the plane. In other words, it is a point to
+/// line constraint with a rotational motor and a linear spring/damper.
 /// This joint is designed for vehicle suspensions.
 class b2WheelJoint : public b2Joint
 {
 public:
-	void GetDefinition(b2WheelJointDef* def) const;
-
 	b2Vec2 GetAnchorA() const;
 	b2Vec2 GetAnchorB() const;
 
@@ -99,8 +96,14 @@ public:
 	/// Get the current joint translation, usually in meters.
 	float32 GetJointTranslation() const;
 
-	/// Get the current joint translation speed, usually in meters per second.
-	float32 GetJointSpeed() const;
+	/// Get the current joint linear speed, usually in meters per second.
+	float32 GetJointLinearSpeed() const;
+
+	/// Get the current joint angle in radians.
+	float32 GetJointAngle() const;
+
+	/// Get the current joint angular speed in radians per second.
+	float32 GetJointAngularSpeed() const;
 
 	/// Is the joint motor enabled?
 	bool IsMotorEnabled() const;
