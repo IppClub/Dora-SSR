53a8996196ce101308ad11708b32db8bf278b6c9
Li Jin <dragon-fly@qq.com>
2022-02-21 09:13:10 +0800

    android activity black screen fix.

--- Project/Android/app/src/main/java/com/luvfight/dorothy/MainActivity.java
+++ Project/Android/app/src/main/java/com/luvfight/dorothy/MainActivity.java
@@ -1,7 +1,13 @@
 package com.luvfight.dorothy;
 
 import org.libsdl.app.SDLActivity;
+
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.Console;
 
 public class MainActivity extends SDLActivity {
 	private static native void nativeSetPath(String path);
@@ -12,5 +18,37 @@ public class MainActivity extends SDLActivity {
 		super.onCreate(savedInstanceState);
 		MainActivity.nativeSetPath(this.getApplicationInfo().sourceDir);
 		MainActivity.nativeSetScreenDensity(this.getResources().getDisplayMetrics().density);
+		hideSystemUI();
+	}
+
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		if (hasFocus) {
+			hideSystemUI();
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		hideSystemUI();
+	}
+
+	private void hideSystemUI() {
+		// Enables regular immersive mode.
+		// For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+		// Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+		View decorView = getWindow().getDecorView();
+		decorView.setSystemUiVisibility(
+				View.SYSTEM_UI_FLAG_IMMERSIVE
+						// Set the content to appear under the system bars so that the
+						// content doesn't resize when the system bars hide and show.
+						| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+						| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+						| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+						// Hide the nav bar and status bar
+						| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+						| View.SYSTEM_UI_FLAG_FULLSCREEN);
 	}
 }
