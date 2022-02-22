34a69e80cc7ee409ab3b871f1ea65f3c062e2dbd
Li Jin <dragon-fly@qq.com>
2022-02-23 00:18:19 +0800

    update script build functions.

--- Assets/Script/init.lua
+++ Assets/Script/init.lua
@@ -2,16 +2,14 @@ local Content = require("Content")
 local Path = require("Path")
 
 Content.searchPaths = {
-	Path(Content.writablePath, "Script"),
-	Path(Content.writablePath, "Script", "Lib"),
+	Path(Content.writablePath, "Build", "Script"),
+	Path(Content.writablePath, "Build", "Script", "Lib"),
 	"Script",
 	Path("Script", "Lib"),
 	Path("Script", "Lib", "Dora"),
 	"Image",
 	"Spine",
 	"DragonBones",
-	"Production",
-	Content.writablePath
 }
 
 local yue = require("yue")
