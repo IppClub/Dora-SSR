af25c280c0bb4310e2d7c4c59c2f4e35aa3d95c2
Li Jin <dragon-fly@qq.com>
2022-03-03 17:00:54 +0800

    cleanup. [skip CI]

--- Source/GUI/ImGuiDora.cpp
+++ Source/GUI/ImGuiDora.cpp
@@ -996,10 +996,10 @@ bool ImGuiDora::init()
 					SDL_Keycode code = event.key.keysym.sym;
 					int key = code & ~SDLK_SCANCODE_MASK;
 					uint16_t mod = event.key.keysym.mod;
-					io.KeyShift = ((mod & KMOD_SHIFT) != 0);
-					io.KeyCtrl = ((mod & KMOD_CTRL) != 0);
-					io.KeyAlt = ((mod & KMOD_ALT) != 0);
-					io.KeySuper = ((mod & KMOD_GUI) != 0);
+					io.AddKeyEvent(ImGuiKey_ModShift, (mod & KMOD_SHIFT) != 0);
+					io.AddKeyEvent(ImGuiKey_ModCtrl, (mod & KMOD_CTRL) != 0);
+					io.AddKeyEvent(ImGuiKey_ModAlt, (mod & KMOD_ALT) != 0);
+					io.AddKeyEvent(ImGuiKey_ModSuper, (mod & KMOD_GUI) != 0);
 					if (_textEditing.empty() || code == SDLK_BACKSPACE || code == SDLK_LEFT || code == SDLK_RIGHT)
 					{
 						if (auto it = _keymap.find(key); it != _keymap.end())
