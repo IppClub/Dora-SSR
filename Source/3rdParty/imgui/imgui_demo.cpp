dd65a78d0f7bf5b9048bb69ba0b27b80aae46b80
Li Jin <dragon-fly@qq.com>
2022-08-31 09:24:15 +0800

    fix release build.

--- Source/3rdParty/imgui/imgui_demo.cpp
+++ Source/3rdParty/imgui/imgui_demo.cpp
@@ -7994,6 +7994,8 @@ void ImGui::ShowAboutWindow(bool*) {}
 void ImGui::ShowDemoWindow(bool*) {}
 void ImGui::ShowUserGuide() {}
 void ImGui::ShowStyleEditor(ImGuiStyle*) {}
+bool ImGui::ShowStyleSelector(char const*) { return false; }
+void ImGui::ShowFontSelector(char const*) {}
 
 #endif
 
