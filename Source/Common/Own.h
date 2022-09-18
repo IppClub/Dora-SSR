571c71d058f00a83fe636ff94e72ea6e29d464ca
Li Jin <dragon-fly@qq.com>
2022-09-18 15:55:23 +0800

    revert.

--- Source/Common/Own.h
+++ Source/Common/Own.h
@@ -11,7 +11,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 NS_DOROTHY_BEGIN
 
 /** @brief Used with Composition Relationship. */
-#define Own std::unique_ptr
+template <class Item, class Del = std::default_delete<Item>>
+using Own = std::unique_ptr<Item, Del>;
 
 template <class Item>
 using OwnArray = std::unique_ptr<Item, std::default_delete<Item[]>>;
