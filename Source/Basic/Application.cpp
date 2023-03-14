194c5d7541bb129428248e22ea2c79472be8b407
Li Jin <dragon-fly@qq.com>
2023-03-14 11:35:00 +0800

    update version.

--- Source/Basic/Application.cpp
+++ Source/Basic/Application.cpp
@@ -22,7 +22,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include "bx/timer.h"
 #include <ctime>
 
-#define DORA_VERSION "1.0.15"_slice
+#define DORA_VERSION "1.0.16"_slice
 
 #if BX_PLATFORM_ANDROID
 #include <jni.h>
