8ea60f6fc079673cd9de0ece7b6650ca8f8cb1cb
Li Jin <dragon-fly@qq.com>
2023-06-02 10:27:28 +0800

    fix wrong param passing to warpToNode. [skip CI]

--- Tools/YarnEditor/src/js/classes/app.js
+++ Tools/YarnEditor/src/js/classes/app.js
@@ -764,7 +764,7 @@ export var App = function(name, version) {
 
 		if (self.settings.editorSplit()) {
 			self.splitEditor();
-			self.workspace.warpToNodeByIdx(node.index() - 1);
+			self.workspace.warpToNodeByIdx(node.index());
 		}
 
 		if (self.settings.editorSplitDirection() === 'right') {
