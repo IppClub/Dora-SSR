b65b9142b1cc7ff07f3933e625508e7f4956d30d
Li Jin <dragon-fly@qq.com>
2023-06-04 19:59:09 +0800

    updating bgfx for Windows.

--- Source/3rdParty/imgui/imgui_demo.cpp
+++ Source/3rdParty/imgui/imgui_demo.cpp
@@ -8046,6 +8046,8 @@ void ImGui::ShowAboutWindow(bool*) {}
 void ImGui::ShowDemoWindow(bool*) {}
 void ImGui::ShowUserGuide() {}
 void ImGui::ShowStyleEditor(ImGuiStyle*) {}
+bool ImGui::ShowStyleSelector(char const*) { return false; }
+void ImGui::ShowFontSelector(char const*) {}
 
 #endif
 
