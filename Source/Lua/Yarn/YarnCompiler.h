dccfa17883647c7f947a22e742f8ec8bfd1036f3
Li Jin <dragon-fly@qq.com>
2023-06-07 12:12:12 +0800

    adding yarn compiler.

--- Source/Lua/Yarn/YarnCompiler.h
+++ Source/Lua/Yarn/YarnCompiler.h
@@ -10,26 +10,4 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 NS_DOROTHY_BEGIN
 
-class XmlResolver {
-public:
-	XmlResolver();
-	~XmlResolver();
-	void resolve(String text);
-	const std::list<std::string>& getImports();
-	const std::string& getCurrentElement();
-	const std::string& getCurrentAttribute();
-	bool isCurrentInTag();
-	int getCurrentPadding();
-
-private:
-	bool parse(const char* codes, int length);
-
-private:
-	bool _isInTag;
-	int _currentPadding;
-	std::string _currentElement;
-	std::string _currentAttribute;
-	std::list<std::string> _imports;
-};
-
 NS_DOROTHY_END
