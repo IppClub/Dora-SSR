43b852692e4acb53b30cd4e0e867ceff4963fce5
Li Jin <dragon-fly@qq.com>
2023-07-03 17:28:17 +0800

    fix Dora Xml auto completion. add docs.

--- Source/3rdParty/Yard/yard_error.hpp
+++ Source/3rdParty/Yard/yard_error.hpp
@@ -6,44 +6,42 @@
 #ifndef YARD_ERROR_HPP
 #define YARD_ERROR_HPP
 
-namespace yard
-{
-    template<typename ParserState_T>
-    void OutputParsingErrorLocation(ParserState_T& p)
-    { 
-        typedef typename ParserState_T::Iterator Iter_T;
-        Iter_T first = p.Begin();
-        Iter_T last = p.End();
-        Iter_T cur = p.GetPos();
-     
-        // Go the beginning of the input, and count the number of newlines,
-        Iter_T tmp = first;
-        Iter_T line_begin = tmp;
-        int line_cnt = 1;
-        while (tmp != cur) {
-            if (*tmp++ == '\n') {
-                line_begin = tmp;
-                line_cnt++;
-            }
-        } 
+namespace yard {
+template <typename ParserState_T>
+void OutputParsingErrorLocation(ParserState_T& p) {
+	typedef typename ParserState_T::Iterator Iter_T;
+	Iter_T first = p.Begin();
+	Iter_T last = p.End();
+	Iter_T cur = p.GetPos();
 
-        // count how many characters between the last newline character and the current position
-        int char_cnt = static_cast<int>(tmp - line_begin);
-        while ((tmp <= last) && (*tmp != '\n')) { 
-            tmp++; 
-        }
+	// Go the beginning of the input, and count the number of newlines,
+	Iter_T tmp = first;
+	Iter_T line_begin = tmp;
+	int line_cnt = 1;
+	while (tmp != cur) {
+		if (*tmp++ == '\n') {
+			line_begin = tmp;
+			line_cnt++;
+		}
+	}
 
-        std::cerr << "error occured on line #" << line_cnt << std::endl;
-        std::cerr << std::string(line_begin, tmp) << std::endl;
-      
-        // this outputs a position pointer 
-        std::cerr << std::string(char_cnt, ' ') << '^' << std::endl;  
-    }
+	// count how many characters between the last newline character and the current position
+	int char_cnt = static_cast<int>(tmp - line_begin);
+	while ((tmp <= last) && (*tmp != '\n')) {
+		tmp++;
+	}
 
-    void UnexpectedEndOfFile() {
-      std::cerr << "unexpected end of input" << std::endl;
-      throw std::runtime_error("unexpected end of input");
-    }
+	std::cerr << "error occured on line #" << line_cnt << std::endl;
+	std::cerr << std::string(line_begin, tmp) << std::endl;
+
+	// this outputs a position pointer
+	std::cerr << std::string(char_cnt, ' ') << '^' << std::endl;
 }
+
+void UnexpectedEndOfFile() {
+	std::cerr << "unexpected end of input" << std::endl;
+	throw std::runtime_error("unexpected end of input");
+}
+} // namespace yard
 
 #endif
--- Source/3rdParty/Yard/yard_parser.hpp
+++ Source/3rdParty/Yard/yard_parser.hpp
@@ -1,173 +1,163 @@
 // Dedicated to the public domain by Christopher Diggins
 // http://www.cdiggins.com
 //
-// Contains definitions for sample parser management classes 
+// Contains definitions for sample parser management classes
 // to be used with the YARD framework
 
 #ifndef YARD_PARSER_HPP
 #define YARD_PARSER_HPP
 
-namespace yard
-{
-    ////////////////////////////////////////////////////////////////////////
-    // A skeleton parser base class
+namespace yard {
+////////////////////////////////////////////////////////////////////////
+// A skeleton parser base class
 
-    template<typename Token_T, typename Iter_T = const Token_T*>
-    struct BasicParser
-    {   
-        // Constructor
-        BasicParser(Iter_T first, Iter_T last) 
-            : mBegin(first), mEnd(last), mIter(first)
-        { }
+template <typename Token_T, typename Iter_T = const Token_T*>
+struct BasicParser {
+	// Constructor
+	BasicParser(Iter_T first, Iter_T last)
+		: mBegin(first)
+		, mEnd(last)
+		, mIter(first) { }
 
-        // Parse function
-        template<typename StartRule_T>
-        bool Parse()
-        {
-            try {
-                return StartRule_T::Match(*this);
-            }
-            catch(...) {
-                return false;
-            }
-        }
+	// Parse function
+	template <typename StartRule_T>
+	bool Parse() {
+		try {
+			return StartRule_T::Match(*this);
+		} catch (...) {
+			return false;
+		}
+	}
 
-        // Public typedefs 
-        typedef Iter_T Iterator;
-        typedef Token_T Token; 
-                
-        // Input pointer functions 
-        Token GetElem() { return *mIter; }  
-        void GotoNext() { assert(mIter < End()); ++mIter; }  
-        Iterator GetPos() { return mIter; }  
-        void SetPos(Iterator pos) { mIter = pos; }  
-        bool AtEnd() { return GetPos() >= End(); }  
-        Iterator Begin() { return mBegin; }    
-        Iterator End() { return mEnd; }  
+	// Public typedefs
+	typedef Iter_T Iterator;
+	typedef Token_T Token;
 
-        template<typename T>
-		void OutputLocation(const T&)
-        { }
+	// Input pointer functions
+	Token GetElem() { return *mIter; }
+	void GotoNext() {
+		assert(mIter < End());
+		++mIter;
+	}
+	Iterator GetPos() { return mIter; }
+	void SetPos(Iterator pos) { mIter = pos; }
+	bool AtEnd() { return GetPos() >= End(); }
+	Iterator Begin() { return mBegin; }
+	Iterator End() { return mEnd; }
 
-    protected:
+	template <typename T>
+	void OutputLocation(const T&) { }
 
-        // Member fields
-        Iterator    mBegin;
-        Iterator    mEnd;
-        Iterator    mIter;
-    };  
-    
-    ////////////////////////////////////////////////////////////////////////
-    // Extends the basic parser with abstract syntax tree (AST) 
-    // building capabilities 
+protected:
+	// Member fields
+	Iterator mBegin;
+	Iterator mEnd;
+	Iterator mIter;
+};
 
-    template<typename Token_T, typename Iter_T = const Token_T*>
-    struct TreeBuildingParser 
-        : BasicParser<Token_T, Iter_T>
-    {   
-        // Constructor
-        TreeBuildingParser(Iter_T first, Iter_T last) 
-            : BasicParser<Token_T, Iter_T>(first, last), mTree()
-        { }
+////////////////////////////////////////////////////////////////////////
+// Extends the basic parser with abstract syntax tree (AST)
+// building capabilities
 
-        // Parse function
-        template<typename StartRule_T>
-        bool Parse()
-        {
-            try {
-                return StartRule_T::Match(*this);
-            }
-            catch(...) {
-                return false;
-            }
-        }
+template <typename Token_T, typename Iter_T = const Token_T*>
+struct TreeBuildingParser
+	: BasicParser<Token_T, Iter_T> {
+	// Constructor
+	TreeBuildingParser(Iter_T first, Iter_T last)
+		: BasicParser<Token_T, Iter_T>(first, last)
+		, mTree() { }
 
-        // Public typedefs 
-        typedef Iter_T Iterator;
-        typedef Token_T Token; 
-        typedef Ast<Iterator> Tree;
-        typedef typename Tree::AbstractNode Node;
-                
-        // AST functions
-        Node* GetAstRoot() { return mTree.GetRoot(); }
-        template<typename Rule_T>
-        void CreateNode() { mTree.template CreateNode<Rule_T>(*this); }
-        void CompleteNode() { mTree.CompleteNode(*this); }
-        void AbandonNode() { mTree.AbandonNode(*this); }
+	// Parse function
+	template <typename StartRule_T>
+	bool Parse() {
+		try {
+			return StartRule_T::Match(*this);
+		} catch (...) {
+			return false;
+		}
+	}
 
-    protected:
+	// Public typedefs
+	typedef Iter_T Iterator;
+	typedef Token_T Token;
+	typedef Ast<Iterator> Tree;
+	typedef typename Tree::AbstractNode Node;
 
-        // Member fields
-        Tree mTree;
-    };  
+	// AST functions
+	Node* GetAstRoot() { return mTree.GetRoot(); }
+	template <typename Rule_T>
+	void CreateNode() { mTree.template CreateNode<Rule_T>(*this); }
+	void CompleteNode() { mTree.CompleteNode(*this); }
+	void AbandonNode() { mTree.AbandonNode(*this); }
 
-    ////////////////////////////////////////////////////////////////////////
-    // A useful simple parser for parsing ascii text
+protected:
+	// Member fields
+	Tree mTree;
+};
 
-    struct SimpleTextParser
-        : TreeBuildingParser<char>
-    {                   
-        // Constructor
-        SimpleTextParser(Iterator first, Iterator last) 
-            : TreeBuildingParser<char>(first, last)
-        { }
-        
-        // Public typedefs 
-        typedef const char* Iterator;
-        typedef char Token; 
-        typedef Tree::AbstractNode Node;
+////////////////////////////////////////////////////////////////////////
+// A useful simple parser for parsing ascii text
 
-        // Parse function
-        template<typename StartRule_T>
-        bool Parse() {
-            try {
-                return StartRule_T::Match(*this);
-            }
-            catch (...) {
-                return false;
-            }
-        }
+struct SimpleTextParser
+	: TreeBuildingParser<char> {
+	// Constructor
+	SimpleTextParser(Iterator first, Iterator last)
+		: TreeBuildingParser<char>(first, last) { }
 
-        template<typename T>
-        void OutputLocation()
-        {
-            char line[256];
-            Iterator pFirst = GetPos();
-            while (pFirst > mBegin && *pFirst != '\n')
-                pFirst--;
-            if (*pFirst == '\n')
-                ++pFirst;
-            Iterator pLast = GetPos();
-            while (pLast < mEnd && *pLast != '\n')
-                pLast++;
-            size_t n = pLast - pFirst;
-            n = n < 254 ? n : 254;
-            strncpy(line, pFirst, n);    
-            line[n] = '\0';
+	// Public typedefs
+	typedef const char* Iterator;
+	typedef char Token;
+	typedef Tree::AbstractNode Node;
 
-            char marker[256];
-            n = GetPos() - pFirst;
-            n = n < 254 ? n : 254;
-            for (size_t i=0; i < n; ++i)
-                marker[i] = ' ';
-            marker[n] = '^';
-            marker[n + 1] = '\0';
+	// Parse function
+	template <typename StartRule_T>
+	bool Parse() {
+		try {
+			return StartRule_T::Match(*this);
+		} catch (...) {
+			return false;
+		}
+	}
 
-            // Count lines  
-            int nline = 1;
-            for (Iterator i = mBegin; i < pFirst; ++i) {
-                if (*i == '\n') {
-                    ++nline;
-                }
-            }
+	template <typename T>
+	void OutputLocation() {
+		char line[256];
+		Iterator pFirst = GetPos();
+		while (pFirst > mBegin && *pFirst != '\n')
+			pFirst--;
+		if (*pFirst == '\n')
+			++pFirst;
+		Iterator pLast = GetPos();
+		while (pLast < mEnd && *pLast != '\n')
+			pLast++;
+		size_t n = pLast - pFirst;
+		n = n < 254 ? n : 254;
+		strncpy(line, pFirst, n);
+		line[n] = '\0';
 
-            printf("character number %d\n", n); 
-            printf("line number %d\n", nline);
-            printf("%s\n", line); 
-            printf("%s\n", marker);
-        }
-    };  
+		char marker[256];
+		n = GetPos() - pFirst;
+		n = n < 254 ? n : 254;
+		for (size_t i = 0; i < n; ++i)
+			marker[i] = ' ';
+		marker[n] = '^';
+		marker[n + 1] = '\0';
 
- }
+		// Count lines
+		int nline = 1;
+		for (Iterator i = mBegin; i < pFirst; ++i) {
+			if (*i == '\n') {
+				++nline;
+			}
+		}
 
-#endif 
+		printf("character number %d\n", n);
+		printf("line number %d\n", nline);
+		printf("%s\n", line);
+		printf("%s\n", marker);
+	}
+};
+
+} // namespace yard
+
+#endif
