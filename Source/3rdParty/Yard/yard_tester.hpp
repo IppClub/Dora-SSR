9e8342e66116f7cdb6af9eaf76fb1b3fcf72232b
Li Jin <dragon-fly@qq.com>
2023-07-03 17:28:17 +0800

    fix Dora Xml auto completion. add docs.

--- Source/3rdParty/Yard/yard_tester.hpp
+++ Source/3rdParty/Yard/yard_tester.hpp
@@ -5,22 +5,19 @@
 #ifndef YARD_TESTER_HPP
 #define YARD_TESTER_HPP
 
-namespace yard_test
-{
-    template<typename Rule_T>
-    bool Test(const char* in)
-    {
-        const char* out = in + strlen(in);
-        yard::SimpleTextParser parser(in, out);
-        bool b = parser.Parse<yard::Seq<Rule_T, yard::EndOfInput> >();
-        if (b) {
-            printf("passed test for rule %s, on input %s\n", typeid(Rule_T).name(), in);
-        }
-        else {
-            printf("FAILED test for rule %s, on input %s\n", typeid(Rule_T).name(), in);
-        }
-        return b;
-    }
+namespace yard_test {
+template <typename Rule_T>
+bool Test(const char* in) {
+	const char* out = in + strlen(in);
+	yard::SimpleTextParser parser(in, out);
+	bool b = parser.Parse<yard::Seq<Rule_T, yard::EndOfInput>>();
+	if (b) {
+		printf("passed test for rule %s, on input %s\n", typeid(Rule_T).name(), in);
+	} else {
+		printf("FAILED test for rule %s, on input %s\n", typeid(Rule_T).name(), in);
+	}
+	return b;
 }
+} // namespace yard_test
 
 #endif
