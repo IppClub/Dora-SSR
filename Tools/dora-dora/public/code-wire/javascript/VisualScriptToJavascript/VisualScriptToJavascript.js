20edc33fc22714dfe0fca98c7c1d3ea91f550d14
Li Jin <dragon-fly@qq.com>
2023-09-08 18:03:51 +0800

    update Visual Script. [skip CI]

--- Tools/dora-dora/public/code-wire/javascript/VisualScriptToJavascript/VisualScriptToJavascript.js
+++ Tools/dora-dora/public/code-wire/javascript/VisualScriptToJavascript/VisualScriptToJavascript.js
@@ -18,6 +18,15 @@ export var VSToJS = class {
 				let codeDoc = document.getElementById("console").contentWindow.document;
 				this.script = '';
 				codeDoc.open();
+				let errDesc = `UnknownException`;
+				switch (err.name) {
+					case 'RangeError':
+						errDesc = 'CyclicDependence : Irresolvable Cycle(s) Exists';
+						break;
+					case 'InputError':
+						errDesc = err.message;
+						break;
+				}
 				codeDoc.writeln(
 					`<!DOCTYPE html>\n
 					<style>
@@ -29,7 +38,7 @@ export var VSToJS = class {
 					<body>
 					<code>
 					Recheck the nodes<br>
-					${err.name === 'RangeError' ? 'CyclicDependence : Irresolvable Cycle(s) Exists' : `UnknownException: Improve The Editor By Opening Issue On GitHub(Just Attach The Exported Graph)`}
+					${errDesc}
 					</code>
 					</body>
 					</html>
@@ -78,13 +87,20 @@ export var VSToJS = class {
 		for (let aNode of node.customClass.inputPins) {
 			if (aNode.wire) {
 				X.push({ node: aNode.wire.attrs.src.getParent(), isWire: true, srcOutputPinNumber: this.getSrcOutputPinNumber(aNode.wire.attrs.src.getParent(), aNode.wire) });
-			}
-			else {
+			} else {
 				// console.log(aNode.textBox);
-				X.push({ node: aNode.textBox.textBox.text(), isWire: false, srcOutputPinNumber: null });
+				X.push({ node: aNode.textBox ? aNode.textBox.textBox.text() : null, isWire: false, srcOutputPinNumber: null });
 			}
 		}
 		return X;
+	}
+	checkInput(nodeId, inputPins, index) {
+		const inputNode = inputPins[index];
+		if (inputNode === undefined || inputNode === null || (!inputNode.wire && (inputNode.node === null || inputNode.node === ''))) {
+			let err = new Error(`node ${nodeId}, expecting value for input pin ${index}`);
+			err.name = "InputError";
+			throw err;
+		}
 	}
 	coreAlgorithm(node) {
 		if (node == null) {
@@ -92,11 +108,13 @@ export var VSToJS = class {
 		}
 		let execOutPins = this.getExecOut(node);
 		let inputPins = this.getInputPins(node);
-		const nl = ` -- ${node._id}\n`;
+		let nodeId = node._id;
+		const nl = ` -- ${nodeId}\n`;
 		// console.log(node.customClass.type);
 		// console.log(inputPins);
 		if (node.customClass.type.isGetSet) {
 			if (node.customClass.type.typeOfNode.slice(0, 3) == 'Set') {
+				this.checkInput(nodeId, inputPins, 0);
 				this.script += this.getIndent() + `${node.customClass.type.typeOfNode.slice(4)} = ${this.handleInputs(inputPins[0])}${nl}`;
 				for (let each of execOutPins) {
 					this.coreAlgorithm(each);
@@ -110,11 +128,13 @@ export var VSToJS = class {
 					break;
 				}
 				case "Print": {
+					this.checkInput(nodeId, inputPins, 0);
 					this.script += this.getIndent() + `print(${this.handleInputs(inputPins[0])})${nl}`;
 					this.coreAlgorithm(execOutPins[0]);
 					break;
 				}
 				case "If/Else": {
+					this.checkInput(nodeId, inputPins, 0);
 					this.script += this.getIndent() + `if ${this.handleInputs(inputPins[0])} then${nl}`;
 					this.indent++;
 					this.coreAlgorithm(execOutPins[0]);
@@ -130,7 +150,10 @@ export var VSToJS = class {
 					break;
 				}
 				case "ForLoop": {
-					let forVar = `i${node._id}`;
+					this.checkInput(nodeId, inputPins, 0);
+					this.checkInput(nodeId, inputPins, 1);
+					this.checkInput(nodeId, inputPins, 2);
+					let forVar = `i${nodeId}`;
 					this.script += this.getIndent() + `for ${forVar} = (${this.handleInputs(inputPins[0])}), (${this.handleInputs(inputPins[1])})`;
 					if (inputPins[2] !== null) {
 						this.script += `, (${this.handleInputs(inputPins[2])}) do${nl}`;
@@ -145,12 +168,23 @@ export var VSToJS = class {
 					break;
 				}
 				case "ForEachLoop": {
-					let forVar = `i${node._id}`;
-					let valueVar = `valuei${node._id}`;
+					this.checkInput(nodeId, inputPins, 0);
+					let forVar = `i${nodeId}`;
+					let valueVar = `valuei${nodeId}`;
 					const arrVar = this.handleInputs(inputPins[0]);
-					this.script += this.getIndent() + `for ${forVar} = 1, #(${arrVar}) do${nl}`;
+					this.script += this.getIndent() + `for ${forVar} = 1, #${arrVar} do${nl}`;
 					this.indent++;
 					this.script += this.getIndent() + `local ${valueVar} = ${arrVar}[${forVar}]${nl}`;
+					this.coreAlgorithm(execOutPins[0]);
+					this.indent--;
+					this.script += this.getIndent() + `end${nl}`;
+					this.coreAlgorithm(execOutPins[1]);
+					break;
+				}
+				case "WhileLoop": {
+					this.checkInput(nodeId, inputPins, 0);
+					this.script += this.getIndent() + `while ${this.handleInputs(inputPins[0])} do${nl}`;
+					this.indent++;
 					this.coreAlgorithm(execOutPins[0]);
 					this.indent--;
 					this.script += this.getIndent() + `end${nl}`;
@@ -161,17 +195,39 @@ export var VSToJS = class {
 					this.script += this.getIndent() + `break${nl}`;
 					break;
 				}
-				case "WhileLoop": {
-					this.script += this.getIndent() + `while ${this.handleInputs(inputPins[0])} do${nl}`;
-					this.indent++;
+				case "SetIndex": {
+					this.checkInput(nodeId, inputPins, 0);
+					this.checkInput(nodeId, inputPins, 1);
+					this.checkInput(nodeId, inputPins, 2);
+					this.script += this.getIndent() + `${this.handleInputs(inputPins[0])}[${this.handleInputs(inputPins[1])}] = ${this.handleInputs(inputPins[2])}${nl}`;
 					this.coreAlgorithm(execOutPins[0]);
-					this.indent--;
-					this.script += this.getIndent() + `end${nl}`;
-					this.coreAlgorithm(execOutPins[1]);
 					break;
 				}
-				case "SetIndex": {
-					this.script += `${this.handleInputs(inputPins[2])}[${this.handleInputs(inputPins[0])}] = ${this.handleInputs(inputPins[1])}${nl}`;
+				case "Append": {
+					this.checkInput(nodeId, inputPins, 0);
+					this.checkInput(nodeId, inputPins, 1);
+					this.script += this.getIndent() + `table.insert(${this.handleInputs(inputPins[0])}, ${this.handleInputs(inputPins[1])})${nl}`;
+					this.coreAlgorithm(execOutPins[0]);
+					break;
+				}
+				case "Insert": {
+					this.checkInput(nodeId, inputPins, 0);
+					this.checkInput(nodeId, inputPins, 1);
+					this.checkInput(nodeId, inputPins, 2);
+					this.script += this.getIndent() + `table.insert(${this.handleInputs(inputPins[0])}, ${this.handleInputs(inputPins[1])}, ${this.handleInputs(inputPins[2])})${nl}`;
+					this.coreAlgorithm(execOutPins[0]);
+					break;
+				}
+				case "RemoveAt": {
+					this.checkInput(nodeId, inputPins, 0);
+					this.checkInput(nodeId, inputPins, 1);
+					this.script += this.getIndent() + `table.remove(${this.handleInputs(inputPins[0])}, ${this.handleInputs(inputPins[1])})${nl}`;
+					this.coreAlgorithm(execOutPins[0]);
+					break;
+				}
+				case "RemoveLast": {
+					this.checkInput(nodeId, inputPins, 0);
+					this.script += this.getIndent() + `table.remove(${this.handleInputs(inputPins[0])})${nl}`;
 					this.coreAlgorithm(execOutPins[0]);
 					break;
 				}
@@ -182,6 +238,7 @@ export var VSToJS = class {
 		if (!inputNode.isWire) {
 			return inputNode.node;
 		}
+		let nodeId = inputNode.node._id;
 		let inputPins = this.getInputPins(inputNode.node);
 		if (inputNode.node.customClass.type.isGetSet) {
 			return `${inputNode.node.customClass.type.typeOfNode.slice(4)}`;
@@ -191,63 +248,101 @@ export var VSToJS = class {
 		// }
 		let expr = ``;
 		switch (inputNode.node.customClass.type.typeOfNode) {
+			case "ForLoop": {
+				expr = `i${nodeId}`;
+				break;
+			}
+			case "ForEachLoop": {
+				if (inputNode.srcOutputPinNumber == 0) {
+					expr = `i${nodeId}`;
+				} else {
+					expr = `valuei${nodeId}`;
+				}
+				break;
+			}
 			case "Add": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} + ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "Subtract": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} - ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "Multiply": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} * ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "Divide": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} / ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "Power": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} ^ ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "Modulo": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} % ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "Ceil": {
+				this.checkInput(nodeId, inputPins, 0);
 				expr = `math.ceil(${this.handleInputs(inputPins[0])})`;
 				break;
 			}
 			case "Floor": {
+				this.checkInput(nodeId, inputPins, 0);
 				expr = `math.floor(${this.handleInputs(inputPins[0])})`;
 				break;
 			}
 			case "And": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} and ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "Or": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} or ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "Not": {
+				this.checkInput(nodeId, inputPins, 0);
 				expr = `not (${this.handleInputs(inputPins[0])})`;
 				break;
 			}
 			case "BitAnd": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} & ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "BitOr": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} | ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "BitXOr": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} ~ ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "BitNeg": {
+				this.checkInput(nodeId, inputPins, 0);
 				expr = `~${this.handleInputs(inputPins[0])}`;
 				break;
 			}
@@ -256,63 +351,91 @@ export var VSToJS = class {
 				break;
 			}
 			case "Equals": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} == ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "Not Equals": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} ~= ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "LessEq": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} <= ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "Less": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} < ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "Greater": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} > ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "GreaterEq": {
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
 				expr = `(${this.handleInputs(inputPins[0])} >= ${this.handleInputs(inputPins[1])})`;
 				break;
 			}
 			case "Length": {
-				expr = `#(${this.handleInputs(inputPins[0])})`;
+				this.checkInput(nodeId, inputPins, 0);
+				expr = `#${this.handleInputs(inputPins[0])}`;
 				break;
 			}
 			case "GetIndex": {
-				expr = `${this.handleInputs(inputPins[1])}[${this.handleInputs(inputPins[0])}]`;
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
+				expr = `${this.handleInputs(inputPins[0])}[${this.handleInputs(inputPins[1])}]`;
 				break;
 			}
 			case "SetIndex": {
-				expr = `${this.handleInputs(inputPins[2])}[${this.handleInputs(inputPins[0])}]`;
+				this.checkInput(nodeId, inputPins, 0);
+				this.checkInput(nodeId, inputPins, 1);
+				expr = `${this.handleInputs(inputPins[0])}[${this.handleInputs(inputPins[1])}]`;
 				break;
 			}
 			case "IsEmpty": {
+				this.checkInput(nodeId, inputPins, 0);
 				expr = `(#${this.handleInputs(inputPins[0])} == 0)`;
 				break;
 			}
 			case "First": {
+				this.checkInput(nodeId, inputPins, 0);
 				expr = `${this.handleInputs(inputPins[0])}[1]`;
 				break;
 			}
 			case "Last": {
+				this.checkInput(nodeId, inputPins, 0);
 				expr = `${this.handleInputs(inputPins[0])}[#${this.handleInputs(inputPins[0])}]`;
 				break;
 			}
-			case "ForLoop": {
-				expr = `i${inputNode.node._id}`;
+			case "Append" : {
+				this.checkInput(nodeId, inputPins, 0);
+				expr = `${this.handleInputs(inputPins[0])}`;
 				break;
 			}
-			case "ForEachLoop": {
-				if (inputNode.srcOutputPinNumber == 0) {
-					expr = `i${inputNode.node._id}`;
-				} else {
-					expr = `valuei${inputNode.node._id}`;
-				}
+			case "Insert": {
+				this.checkInput(nodeId, inputPins, 0);
+				expr = `${this.handleInputs(inputPins[0])}`;
+				break;
+			}
+			case "RemoveAt": {
+				this.checkInput(nodeId, inputPins, 0);
+				expr = `${this.handleInputs(inputPins[0])}`;
+				break;
+			}
+			case "RemoveLast": {
+				this.checkInput(nodeId, inputPins, 0);
+				expr = `${this.handleInputs(inputPins[0])}`;
 				break;
 			}
 		}
--- Tools/dora-dora/public/code-wire/javascript/VisualScriptToJavascript/liveCode.js
+++ Tools/dora-dora/public/code-wire/javascript/VisualScriptToJavascript/liveCode.js
@@ -1,18 +1,18 @@
 let myCodeMirror = CodeMirror(document.getElementById("live-code"), {
-    value: '',
-    mode: "lua",
-    lineNumbers: true,
-    readOnly: true,
-    theme: 'material-darker',
-    viewportMargin: 10,
-    indentUnit: 4,
+	value: '',
+	mode: "lua",
+	lineNumbers: true,
+	readOnly: true,
+	theme: 'material-darker',
+	viewportMargin: 10,
+	indentUnit: 4,
 });
 myCodeMirror.setSize("100%", "100%");
 export function refresh(script) {
-    // myCodeMirror.cm.height = 600;
-    // console.log(myCodeMirror);
-    myCodeMirror.setValue(script);
-    requestAnimationFrame(() => {})
-    myCodeMirror.setCursor({ line: 0, ch: 0 });
-    // console.log(script);
+	// myCodeMirror.cm.height = 600;
+	// console.log(myCodeMirror);
+	myCodeMirror.setValue(script);
+	requestAnimationFrame(() => {})
+	myCodeMirror.setCursor({ line: 0, ch: 0 });
+	// console.log(script);
 }
