d94751663d8b41bdc0de44e6fbd5a7c2cb72d617
Li Jin <dragon-fly@qq.com>
2023-10-18 10:14:41 +0800

    fix cpp utf8 literals issue.

--- Source/3rdParty/yuescript/yue_compiler.cpp
+++ Source/3rdParty/yuescript/yue_compiler.cpp
@@ -74,7 +74,7 @@ static std::unordered_set<std::string> Metamethods = {
 	"close"s // Lua 5.4
 };
 
-const std::string_view version = "0.19.5"sv;
+const std::string_view version = "0.19.6"sv;
 const std::string_view extension = "yue"sv;
 
 class CompileError : public std::logic_error {
