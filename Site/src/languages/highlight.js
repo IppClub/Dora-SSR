a300d0e88a0dbf4d0ad98a535da45f448bf0ef63
Li Jin <dragon-fly@qq.com>
2023-11-16 16:48:14 +0800

    update web deps. [skip CI]

--- Site/src/languages/highlight.js
+++ Site/src/languages/highlight.js
@@ -1,4 +1,4 @@
-import Prism from "prism-react-renderer/prism";
+import { Prism } from "prism-react-renderer";
 import prismYuescript from './yuescript-prism';
 import prismTeal from './teal-prism';
 import prismLua from './lua-prism';
