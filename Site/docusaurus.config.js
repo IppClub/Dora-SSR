b82a4b90fd91601eb0bf1ba8352ff0ef91ae1e3a
Li Jin <dragon-fly@qq.com>
2023-12-06 23:05:21 +0800

    update docs. [skip CI]

--- Site/docusaurus.config.js
+++ Site/docusaurus.config.js
@@ -1,9 +1,170 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-import { themes } from "prism-react-renderer";
-const lightCodeTheme = themes.vsLight;
-const darkCodeTheme = themes.vsDark;
+import { themes } from 'prism-react-renderer';
+
+const darkCodeTheme = {
+	plain: {
+		color: '#D4D4D4',
+		backgroundColor: '#212121',
+	},
+	styles: [
+		...themes.vsDark.styles,
+		{
+			types: ['title'],
+			style: {
+			color: '#569CD6',
+			fontWeight: 'bold',
+			},
+		},
+		{
+			types: ['property', 'parameter'],
+			style: {
+			color: '#9CDCFE',
+			},
+		},
+		{
+			types: ['script'],
+			style: {
+			color: '#D4D4D4',
+			},
+		},
+		{
+			types: ['boolean', 'arrow', 'atrule', 'tag'],
+			style: {
+			color: '#569CD6',
+			},
+		},
+		{
+			types: ['number', 'color', 'unit'],
+			style: {
+			color: '#B5CEA8',
+			},
+		},
+		{
+			types: ['font-matter'],
+			style: {
+			color: '#CE9178',
+			},
+		},
+		{
+			types: ['keyword', 'rule'],
+			style: {
+			color: '#C586C0',
+			},
+		},
+		{
+			types: ['regex'],
+			style: {
+			color: '#D16969',
+			},
+		},
+		{
+			types: ['maybe-class-name'],
+			style: {
+			color: '#4EC9B0',
+			},
+		},
+		{
+			types: ['constant'],
+			style: {
+			color: '#4FC1FF',
+			},
+		},
+	],
+};
+
+const lightCodeTheme = {
+	...themes.github,
+	styles: [
+		...themes.github.styles,
+		{
+			types: ['title'],
+			style: {
+				color: '#0550AE',
+				fontWeight: 'bold',
+			},
+		},
+		{
+			types: ['parameter'],
+			style: {
+				color: '#953800',
+			},
+		},
+		{
+			types: ['boolean', 'rule', 'color', 'number', 'constant', 'property'],
+			style: {
+				color: '#005CC5',
+			},
+		},
+		{
+			types: ['atrule', 'tag'],
+			style: {
+				color: '#22863A',
+			},
+		},
+		{
+			types: ['script'],
+			style: {
+				color: '#24292E',
+			},
+		},
+		{
+			types: ['operator', 'unit', 'rule'],
+			style: {
+				color: '#D73A49',
+			},
+		},
+		{
+			types: ['font-matter', 'string', 'attr-value'],
+			style: {
+				color: '#C6105F',
+			},
+		},
+		{
+			types: ['class-name'],
+			style: {
+				color: '#116329',
+			},
+		},
+		{
+			types: ['attr-name'],
+			style: {
+				color: '#0550AE',
+			},
+		},
+		{
+			types: ['keyword'],
+			style: {
+				color: '#CF222E',
+			},
+		},
+		{
+			types: ['function'],
+			style: {
+				color: '#8250DF',
+			},
+		},
+		{
+			types: ['selector'],
+			style: {
+				color: '#6F42C1',
+			},
+		},
+		{
+			types: ['variable'],
+			style: {
+				color: '#E36209',
+			},
+		},
+		{
+			types: ['comment'],
+			style: {
+				color: '#6B6B6B',
+			},
+		},
+	],
+};
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -44,7 +205,7 @@ const config = {
 					// Remove this to remove the "edit this page" links.
 					editUrl:
 						'https://github.com/pigpigyyy/Dorothy-SSR/tree/main/Site',
-					sidebarCollapsed: false,
+					// sidebarCollapsed: false,
 				},
 				blog: {
 					showReadingTime: true,
