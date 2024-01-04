36863c17d0f4731fbe22a33762ae742d85b91dde
Li Jin <dragon-fly@qq.com>
2024-01-04 10:32:28 +0800

    update Year.

--- Source/Lua/ToLua/tolua++.h
+++ Source/Lua/ToLua/tolua++.h
@@ -1,6 +1,6 @@
 /* tolua
 ** Support code for Lua bindings.
-** Written by Waldemar Celes, modified by Jin Li, 2021
+** Written by Waldemar Celes, modified by Li Jin, 2021
 ** TeCGraf/PUC-Rio
 ** Apr 2003, Apr 2014
 ** $Id: $
