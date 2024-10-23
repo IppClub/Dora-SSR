3069a77b55dbcebceed4664164b9abd0a2e6b6a3
Li Jin <dragon-fly@qq.com>
2024-10-23 18:09:58 +0800

    Refactored some tests. [skip CI]

--- Assets/Script/Test/UI-TSX.lua
+++ Assets/Script/Test/UI-TSX.lua
@@ -8,11 +8,12 @@ local ____Dora = require("Dora") -- 3
 local Size = ____Dora.Size -- 3
 local sleep = ____Dora.sleep -- 3
 local thread = ____Dora.thread -- 3
+local tolua = ____Dora.tolua -- 3
 local Vec2 = ____Dora.Vec2 -- 3
 local ____Utils = require("Utils") -- 4
 local Struct = ____Utils.Struct -- 4
-local ButtonCreate = require("UI.Control.Basic.Button") -- 6
-local LineRectCreate = require("UI.View.Shape.LineRect") -- 8
+local LineRectCreate = require("UI.View.Shape.LineRect") -- 6
+local ButtonCreate = require("UI.Control.Basic.Button") -- 7
 local ScrollAreaCreate = require("UI.Control.Basic.ScrollArea") -- 9
 local function Button(props) -- 20
 	return React.createElement( -- 21
@@ -63,85 +64,89 @@ local scrollArea = useRef() -- 78
 local items = Array() -- 80
 items.__added = function(index, item) -- 81
 	local current = scrollArea.current -- 81
-	if current then -- 81
-		local node = toNode(React.createElement( -- 84
-			Button, -- 85
-			{ -- 85
-				text = item.name, -- 85
-				width = 50, -- 85
-				height = 50, -- 85
-				onClick = function() -- 85
-					thread(function() -- 86
-						sleep(0.5) -- 87
-						items:remove(item) -- 88
-					end) -- 86
-				end -- 85
-			} -- 85
-		)) -- 85
-		if node then -- 85
-			node.visible = false -- 93
-			node.x = -1000 -- 94
-			node:addTo(current.view, index) -- 95
-		end -- 95
+	if not current then -- 81
+		return -- 83
+	end -- 83
+	local node = toNode(React.createElement( -- 84
+		Button, -- 85
+		{ -- 85
+			text = item.name, -- 85
+			width = 50, -- 85
+			height = 50, -- 85
+			onClick = function() -- 85
+				thread(function() -- 86
+					sleep(0.5) -- 87
+					items:remove(item) -- 88
+				end) -- 86
+			end -- 85
+		} -- 85
+	)) -- 85
+	if node then -- 85
+		node.visible = false -- 93
+		node.x = -1000 -- 94
+		node:addTo(current.view, index) -- 95
 	end -- 95
 end -- 81
-items.__removed = function(index) -- 99
-	local current = scrollArea.current -- 99
-	if current then -- 99
-		local ____opt_3 = current.view.children -- 99
-		local child = ____opt_3 and ____opt_3:get(index) -- 102
-		if child then -- 102
-			child:removeFromParent() -- 104
-		end -- 104
-	end -- 104
-end -- 99
-items.__updated = function() -- 108
-	local current = scrollArea.current -- 108
-	if current then -- 108
-		current:adjustSizeWithAlign("Auto") -- 111
-	end -- 111
-end -- 108
-toNode(React.createElement( -- 115
-	"align-node", -- 115
-	{windowRoot = true, style = {alignItems = "center", justifyContent = "center"}}, -- 115
-	React.createElement( -- 115
-		"align-node", -- 115
-		{ -- 115
-			style = {width = "50%", height = "50%"}, -- 115
-			onLayout = function(width, height) -- 115
-				local current = scrollArea.current -- 115
-				if current then -- 115
-					current.position = Vec2(width / 2, height / 2) -- 120
-					current:adjustSizeWithAlign( -- 121
-						"Auto", -- 121
-						10, -- 121
-						Size(width, height) -- 121
-					) -- 121
-					local ____opt_5 = current:getChildByTag("border") -- 121
-					if ____opt_5 ~= nil then -- 121
-						____opt_5:removeFromParent() -- 122
-					end -- 122
-					local border = LineRectCreate({ -- 123
-						x = -width / 2, -- 123
-						y = -height / 2, -- 123
-						width = width, -- 123
-						height = height, -- 123
-						color = 4294967295 -- 123
-					}) -- 123
-					current:addChild(border, 0, "border") -- 124
-				end -- 124
-			end -- 117
-		}, -- 117
-		React.createElement(ScrollArea, {ref = scrollArea, width = 250, height = 300, paddingX = 0}) -- 117
-	) -- 117
-)) -- 117
-thread(function() -- 132
-	for i = 1, 30 do -- 132
-		items:insert(Item({ -- 134
-			name = "btn " .. tostring(i), -- 134
-			value = i -- 134
-		})) -- 134
-		sleep(1) -- 135
-	end -- 135
-end) -- 132
-return ____exports -- 132
+items.__removed = function(index) -- 98
+	local current = scrollArea.current -- 98
+	local ____tolua_cast_7 = tolua.cast -- 100
+	local ____opt_3 = current and current.view.children -- 100
+	local child = ____tolua_cast_7( -- 100
+		____opt_3 and ____opt_3:get(index), -- 100
+		"Node" -- 100
+	) -- 100
+	if child then -- 100
+		child:removeFromParent() -- 101
+	end -- 101
+end -- 98
+items.__updated = function() -- 103
+	local current = scrollArea.current -- 103
+	if current ~= nil then -- 103
+		current:adjustSizeWithAlign("Auto") -- 105
+	end -- 105
+end -- 103
+toNode(React.createElement( -- 108
+	"align-node", -- 108
+	{windowRoot = true, style = {alignItems = "center", justifyContent = "center"}}, -- 108
+	React.createElement( -- 108
+		"align-node", -- 108
+		{ -- 108
+			style = {width = "50%", height = "50%"}, -- 108
+			onLayout = function(width, height) -- 108
+				local current = scrollArea.current -- 108
+				if not current then -- 108
+					return -- 112
+				end -- 112
+				current.position = Vec2(width / 2, height / 2) -- 113
+				current:adjustSizeWithAlign( -- 114
+					"Auto", -- 114
+					10, -- 114
+					Size(width, height) -- 114
+				) -- 114
+				local ____opt_10 = current:getChildByTag("border") -- 114
+				if ____opt_10 ~= nil then -- 114
+					____opt_10:removeFromParent() -- 115
+				end -- 115
+				local border = LineRectCreate({ -- 116
+					x = -width / 2, -- 116
+					y = -height / 2, -- 116
+					width = width, -- 116
+					height = height, -- 116
+					color = 4294967295 -- 116
+				}) -- 116
+				current:addChild(border, 0, "border") -- 117
+			end -- 110
+		}, -- 110
+		React.createElement(ScrollArea, {ref = scrollArea, width = 250, height = 300, paddingX = 0}) -- 110
+	) -- 110
+)) -- 110
+thread(function() -- 124
+	for i = 1, 30 do -- 124
+		items:insert(Item({ -- 126
+			name = "btn " .. tostring(i), -- 126
+			value = i -- 126
+		})) -- 126
+		sleep(1) -- 127
+	end -- 127
+end) -- 124
+return ____exports -- 124
