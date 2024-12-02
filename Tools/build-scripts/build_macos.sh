b27a14cb5d336474f318700dee1d8df497289831
Li Jin <dragon-fly@qq.com>
2024-12-02 14:17:21 +0800

    Updated build scripts. [skip CI]

--- Tools/build-scripts/build_macos.sh
+++ Tools/build-scripts/build_macos.sh
@@ -2,7 +2,11 @@
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "$SCRIPT_DIR/../../Source/Rust"
+rustup target add aarch64-apple-darwin
+rustup target add x86_64-apple-darwin
 cargo build --release --target aarch64-apple-darwin
 cargo build --release --target x86_64-apple-darwin
 lipo -create target/aarch64-apple-darwin/release/libdora_runtime.a target/x86_64-apple-darwin/release/libdora_runtime.a -output lib/macOS/libdora_runtime.a
-xcodebuild ONLY_ACTIVE_ARCH=NO -project ../../Projects/macOS/Dora.xcodeproj -target Dora -configuration Release CONFIGURATION_BUILD_DIR=./build/Release
+xcodebuild archive -project ../../Projects/macOS/Dora.xcodeproj -scheme Dora -configuration Release -archivePath ../../Projects/macOS/build/Release/dora.xcarchive -arch arm64 -arch x86_64 ONLY_ACTIVE_ARCH=NO
+
+echo "Built APP in 'Projects/macOS/build/Release/dora.xcarchive/Products/Applications'"
--- Tools/build-scripts/build_windows.bat
+++ Tools/build-scripts/build_windows.bat
@@ -1,7 +1,10 @@
 set SCRIPT_DIR=%~dp0
 set SUBDIR_PATH=%SCRIPT_DIR%..\..\Source\Rust
 cd %SUBDIR_PATH%
+rustup target add i686-pc-windows-msvc
 cargo build --release --target i686-pc-windows-msvc
 copy target\i686-pc-windows-msvc\release\dora_runtime.lib lib\Windows\dora_runtime.lib
 msbuild ..\..\Projects\Windows\Dora.sln -p:Configuration=release
+
+echo Built APP in 'Projects\Windows\build\Release'
 
