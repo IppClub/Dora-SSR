a6a51f7d1e779e6c9b5499136595cbe9dfbfe44d
Li Jin <dragon-fly@qq.com>
2025-04-25 17:39:12 +0800

    Updated revision.

--- Tools/dora-dora/src/Markdown.tsx
+++ Tools/dora-dora/src/Markdown.tsx
@@ -31,60 +31,61 @@ export interface MarkdownProps {
 };
 
 const Markdown = memo((props: MarkdownProps) => {
-	return <ReactMarkdown
-		className='markdown-body'
-		children={props.content}
-		remarkPlugins={[remarkGfm]}
-		components={{
-			img({node, src, alt, ...iprops}) {
-				const {path} = props;
-				const tokens = (alt ?? "").split(':');
-				let width: number | undefined = undefined;
-				let height: number | undefined = undefined;
-				if (tokens.length === 2) {
-					const size = tokens[tokens.length - 1].split('x');
-					if (size.length === 1) {
-						width = Number.parseFloat(size[0]);
-					} else if (size.length === 2) {
-						width = Number.parseFloat(size[0]);
-						height = Number.parseFloat(size[1]);
+	return <div className="markdown-body">
+		<ReactMarkdown
+			children={props.content}
+			remarkPlugins={[remarkGfm]}
+			components={{
+				img({node, src, alt, ...iprops}) {
+					const {path} = props;
+					const tokens = (alt ?? "").split(':');
+					let width: number | undefined = undefined;
+					let height: number | undefined = undefined;
+					if (tokens.length === 2) {
+						const size = tokens[tokens.length - 1].split('x');
+						if (size.length === 1) {
+							width = Number.parseFloat(size[0]);
+						} else if (size.length === 2) {
+							width = Number.parseFloat(size[0]);
+							height = Number.parseFloat(size[1]);
+						}
 					}
-				}
-				return <img src={path === "" ? src : path + "/" + src} alt={alt} width={width} height={height} {...iprops}/>;
-			},
-			a({node, href, ...aprops}) {
-				if (href?.match("^http")) {
+					return <img src={path === "" ? src : path + "/" + src} alt={alt} width={width} height={height} {...iprops}/>;
+				},
+				a({node, href, ...aprops}) {
+					if (href?.match("^http")) {
+						// eslint-disable-next-line
+						return <a href={href} target="_blank" rel="noreferrer" {...aprops}/>;
+					}
 					// eslint-disable-next-line
-					return <a href={href} target="_blank" rel="noreferrer" {...aprops}/>;
+					return <a href='#!' onClick={(e)=> {
+						e.preventDefault();
+						if (node?.properties !== undefined && node.properties.href !== undefined && typeof(node.properties.href) === "string") {
+							props.onClick(node.properties.href, props.fileKey);
+						}
+					}} {...aprops}/>;
+				},
+				code({node, className, children, ...props}) {
+					const match = /language-(\w+)/.exec(className || '');
+					return match ? (
+						<SyntaxHighlighter
+							children={String(children).replace(/\n$/, '')}
+							style={vscDarkPlus as any}
+							customStyle={{
+								backgroundColor: '#161b22'
+							}}
+							language={match[1]}
+							PreTag="div"
+						/>
+					) : (
+						<code className={className} {...props}>
+							{children}
+						</code>
+					);
 				}
-				// eslint-disable-next-line
-				return <a href='#!' onClick={(e)=> {
-					e.preventDefault();
-					if (node?.properties !== undefined && node.properties.href !== undefined && typeof(node.properties.href) === "string") {
-						props.onClick(node.properties.href, props.fileKey);
-					}
-				}} {...aprops}/>;
-			},
-			code({node, className, children, ...props}) {
-				const match = /language-(\w+)/.exec(className || '');
-				return match ? (
-					<SyntaxHighlighter
-						children={String(children).replace(/\n$/, '')}
-						style={vscDarkPlus as any}
-						customStyle={{
-							backgroundColor: '#161b22'
-						}}
-						language={match[1]}
-						PreTag="div"
-					/>
-				) : (
-					<code className={className} {...props}>
-						{children}
-					</code>
-				);
-			}
-		}}
-	/>;
+			}}
+		/>
+	</div>;
 });
 
 export default Markdown;
