ed022b2d6d8a3406122df072bb9cb4c948c1b568
Li Jin <dragon-fly@qq.com>
2025-06-25 08:59:20 +0800

    Fixed WASM version. [skip CI]

--- Source/Wasm/WasmRuntime.cpp
+++ Source/Wasm/WasmRuntime.cpp
@@ -102,7 +102,7 @@ NS_DORA_BEGIN
 
 #define DoraVersion(major, minor, patch) ((major) << 16 | (minor) << 8 | (patch))
 
-static const int doraWASMVersion = DoraVersion(0, 4, 25);
+static const int doraWASMVersion = DoraVersion(0, 4, 26);
 
 static std::string VersionToStr(int version) {
 	return std::to_string((version & 0x00ff0000) >> 16) + '.' + std::to_string((version & 0x0000ff00) >> 8) + '.' + std::to_string(version & 0x000000ff);
