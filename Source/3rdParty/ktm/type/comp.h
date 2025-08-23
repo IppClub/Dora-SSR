0b2f6b90e5c448390311e72a5227466d06758086
YGXXD <764863366@qq.com>
2025-08-23 12:49:23 +0800

    update ktm version
    Signed-off-by: YGXXD <764863366@qq.com>

--- Source/3rdParty/ktm/type/comp.h
+++ Source/3rdParty/ktm/type/comp.h
@@ -14,6 +14,7 @@
 #include "../interface/shared/iarray_util.h"
 #include "../interface/shared/iarray_calc.h"
 #include "../interface/shared/iarray_io.h"
+#include "../interface/shared/iarray_tostring.h"
 #include "../interface/complex/icomp_data.h"
 #include "../interface/complex/icomp_make.h"
 #include "../interface/complex/icomp_array.h"
@@ -23,13 +24,13 @@ namespace ktm
 {
 
 template <class Child>
-using comp_father_type = single_extends_t<Child, icomp_data, icomp_make, icomp_array, icomp_mul, iarray_io,
-                                          iarray_madd_scalar, iarray_mul_scalar, iarray_add, iarray_util>;
+using comp_components = single_extends_t<Child, icomp_data, icomp_make, icomp_array, icomp_mul, iarray_tostring,
+                                         iarray_io, iarray_madd_scalar, iarray_mul_scalar, iarray_add, iarray_util>;
 
 template <typename T>
-struct comp<T> : comp_father_type<comp<T>>
+struct comp<T> : comp_components<comp<T>>
 {
-    using fater_type = comp_father_type<comp<T>>;
+    using fater_type = comp_components<comp<T>>;
     using fater_type::fater_type;
 };
 
