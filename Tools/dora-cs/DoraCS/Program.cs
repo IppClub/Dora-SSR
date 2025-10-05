02bf096b3587df0c9b795955b8b4ad37002bac62
Li Jin <dragon-fly@qq.com>
2025-10-06 07:11:41 +0800

    Updated C# bindings. [skip CI]

--- Tools/dora-cs/DoraCS/Program.cs
+++ Tools/dora-cs/DoraCS/Program.cs
@@ -11,14 +11,17 @@ App.Run(() =>
         ActionDef.Scale(1.0f, 0.3f, 0.5f, EaseType.OutBack),
         ActionDef.Scale(1.0f, 0.5f, 0.3f, EaseType.InBack),
     ]), true);
-    node.Schedule(Co.Once(MyCoroutine));
+    node.Schedule(Co.Loop(MyCoroutine));
     IEnumerator MyCoroutine()
     {
         Log.Print("start");
+        var awaiter = Content.LoadAsync("non-existed.txt");
+        yield return awaiter;
+        Log.Print(awaiter.Result);
         yield return new WaitForSeconds(3.0);
         Log.Print("after 3s");
-        node.RemoveFromParent(true);
-        Log.Print("destroyed sprite");
+        //node.RemoveFromParent(true);
+        //Log.Print("destroyed sprite");
     }
     node.OnTapped((touch) =>
     {
