0eff4fb7eb0d007ea1394649d244bdc5686e6b8e
Li Jin <dragon-fly@qq.com>
2025-10-24 11:39:14 +0800

    Updated TSTL to v1.32.0. [skip CI]

--- Tools/dora-dora/src/3rdParty/tstl/transformation/visitors/access.ts
+++ Tools/dora-dora/src/3rdParty/tstl/transformation/visitors/access.ts
@@ -115,7 +115,7 @@ export function transformPropertyAccessExpressionWithCapture(
 
     let property = node.name.text;
     const symbol = context.checker.getSymbolAtLocation(node.name);
-    const customName = getCustomNameFromSymbol(symbol);
+    const customName = getCustomNameFromSymbol(context, symbol);
     if (customName) {
         property = customName;
     }
@@ -221,4 +221,3 @@ export const transformQualifiedName: FunctionVisitor<ts.QualifiedName> = (node, 
 
     return lua.createTableIndexExpression(left, right, node);
 };
-
