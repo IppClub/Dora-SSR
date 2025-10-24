3e9d01a163b0dac4571e71d0e22c749bf141999d
Li Jin <dragon-fly@qq.com>
2025-10-24 10:54:10 +0800

    Updated version. [skip CI]

--- Source/Basic/Application.cpp
+++ Source/Basic/Application.cpp
@@ -29,8 +29,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <ctime>
 #include <thread>
 
-#define DORA_VERSION "1.7.1"_slice
-#define DORA_REVISION "3"_slice
+#define DORA_VERSION "1.7.2"_slice
+#define DORA_REVISION "1"_slice
 
 #if BX_PLATFORM_ANDROID
 #include <jni.h>
