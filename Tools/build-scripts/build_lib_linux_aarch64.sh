330e1526d82f037cd189f24435a90dea305fae9a
Li Jin <dragon-fly@qq.com>
2025-10-28 14:58:23 +0800

    Cleanup Linux build script. [skip CI]

--- Tools/build-scripts/build_lib_linux_aarch64.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$SCRIPT_DIR/../../Source/Rust"
-
-rustup target add aarch64-unknown-linux-gnu
-cargo build --release --target aarch64-unknown-linux-gnu
-cp target/aarch64-unknown-linux-gnu/release/libdora_runtime.a lib/Linux/libdora_runtime.a
-
--- Tools/build-scripts/build_lib_linux_x86_64.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-cd "$SCRIPT_DIR/../../Source/Rust"
-
-rustup target add x86_64-unknown-linux-gnu
-cargo build --release --target x86_64-unknown-linux-gnu
-cp target/x86_64-unknown-linux-gnu/release/libdora_runtime.a lib/Linux/libdora_runtime.a
-
