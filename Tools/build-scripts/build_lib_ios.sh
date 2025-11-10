bcac0edb32b33133aa82b348cc8501a0c07366c0
Li Jin <dragon-fly@qq.com>
2025-11-10 14:46:35 +0800

    Updated ios build script.

--- Tools/build-scripts/build_lib_ios.sh
+++ Tools/build-scripts/build_lib_ios.sh
@@ -5,8 +5,9 @@ cd "$SCRIPT_DIR/../../Source/Rust"
 
 rustup target add aarch64-apple-ios
 rustup target add aarch64-apple-ios-sim
+rustup target add x86_64-apple-ios
 cargo build --release --target aarch64-apple-ios
 cp target/aarch64-apple-ios/release/libdora_runtime.a lib/iOS/libdora_runtime.a
 cargo build --release --target aarch64-apple-ios-sim
-cp target/aarch64-apple-ios-sim/release/libdora_runtime.a lib/iOS-Simulator/libdora_runtime.a
-
+cargo build --release --target x86_64-apple-ios
+lipo -create target/aarch64-apple-ios-sim/release/libdora_runtime.a target/x86_64-apple-ios/release/libdora_runtime.a -output lib/iOS-Simulator/libdora_runtime.a
