b57476b29535e3fb5be34009ea0e525c399ebb7d
Li Jin <dragon-fly@qq.com>
2025-12-24 23:59:14 +0800

    Updated YueScript version. [skip CI]

--- Source/3rdParty/yuescript/yue_compiler.cpp
+++ Source/3rdParty/yuescript/yue_compiler.cpp
@@ -78,7 +78,7 @@ static std::unordered_set<std::string> Metamethods = {
 	"close"s // Lua 5.4
 };
 
-const std::string_view version = "0.30.3"sv;
+const std::string_view version = "0.30.4"sv;
 const std::string_view extension = "yue"sv;
 
 class CompileError : public std::logic_error {
