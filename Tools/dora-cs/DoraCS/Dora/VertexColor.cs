7c86b84b34935f00bee815626766942c763e0b1c
Li Jin <dragon-fly@qq.com>
2026-01-07 16:23:40 +0800

    Bumped year. [skip CI]

--- Tools/dora-cs/DoraCS/Dora/VertexColor.cs
+++ Tools/dora-cs/DoraCS/Dora/VertexColor.cs
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016-2025 Li Jin <dragon-fly@qq.com>
+/* Copyright (c) 2016-2026 Li Jin <dragon-fly@qq.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -48,13 +48,13 @@ namespace Dora
 		{
 			return new VertexColor(raw);
 		}
-		
+
 		public Vec2 Vertex
 		{
 			set => Native.vertexcolor_set_vertex(Raw, value.Raw);
 			get => Vec2.From(Native.vertexcolor_get_vertex(Raw));
 		}
-		
+
 		public Color Color
 		{
 			set => Native.vertexcolor_set_color(Raw, (int)value.ToARGB());
