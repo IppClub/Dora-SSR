75474330798ac5ed60a28d8dddd20f9121f86d10
Li Jin <dragon-fly@qq.com>
2026-02-02 15:55:09 +0800

    Added utf-8 support for `tiny_regex.h`. [skip CI]

--- Assets/Script/Lib/Dora/en/Dora.d.ts
+++ Assets/Script/Lib/Dora/en/Dora.d.ts
@@ -3719,6 +3719,22 @@ class Content {
 	 * @param excludes An array of directory names to skip during searching.
 	 * @param pattern The search pattern.
 	 * @param useRegex Whether to treat pattern as regex (default false).
+	 * Regex supports:
+	 * '.'        Dot, matches any character
+	 * '^'        Start anchor, matches beginning of string
+	 * '$'        End anchor, matches end of string
+	 * '*'        Asterisk, match zero or more (greedy)
+	 * '+'        Plus, match one or more (greedy)
+	 * '?'        Question, match zero or one (non-greedy)
+	 * '[abc]'    Character class, match if one of {'a', 'b', 'c'}
+	 * '[^abc]'   Inverted class, match if NOT one of {'a', 'b', 'c'}
+	 * '[a-zA-Z]' Character ranges, the character set of the ranges { a-z | A-Z }
+	 * '\s'       Whitespace, \t \f \r \n \v and spaces
+	 * '\S'       Non-whitespace
+	 * '\w'       Alphanumeric, [a-zA-Z0-9_]
+	 * '\W'       Non-alphanumeric
+	 * '\d'       Digits, [0-9]
+	 * '\D'       Non-digits
 	 * @param caseSensitive Whether to use case-sensitive matching (default false).
 	 * @param includeContent Whether to include the matched content snippet (default false).
 	 * @param contentWindow Number of characters around the match to include when includeContent is true.
