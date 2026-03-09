e254abb0bb7332de3f8b246d9a81f209f3c183e9
Li Jin <dragon-fly@qq.com>
2026-03-09 11:30:07 +0800

    Fixed Linux build.

--- Source/3rdParty/bgfx/src/config.h
+++ Source/3rdParty/bgfx/src/config.h
@@ -37,7 +37,6 @@
 
 #	ifndef BGFX_CONFIG_RENDERER_DIRECT3D11
 #		define BGFX_CONFIG_RENDERER_DIRECT3D11 (0 \
-					|| BX_PLATFORM_LINUX          \
 					|| BX_PLATFORM_WINDOWS        \
 					|| BX_PLATFORM_WINRT          \
 					|| BX_PLATFORM_XBOXONE        \
@@ -46,7 +45,6 @@
 
 #	ifndef BGFX_CONFIG_RENDERER_DIRECT3D12
 #		define BGFX_CONFIG_RENDERER_DIRECT3D12 (0 \
-					|| BX_PLATFORM_LINUX          \
 					|| BX_PLATFORM_WINDOWS        \
 					|| BX_PLATFORM_WINRT          \
 					|| BX_PLATFORM_XBOXONE        \
