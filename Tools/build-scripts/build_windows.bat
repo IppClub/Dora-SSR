6fbbfb088ba4c2b2b5687211b1248f1ab56efe11
Li Jin <dragon-fly@qq.com>
2026-05-22 19:23:32 +0800

    Fixed Windows build. [skip CI]

--- Tools/build-scripts/build_windows.bat
+++ Tools/build-scripts/build_windows.bat
@@ -6,8 +6,15 @@ if errorlevel 1 exit /b %errorlevel%
 set SUBDIR_PATH=%SCRIPT_DIR%..\..\Source\Rust
 cd /d %SUBDIR_PATH%
 rustup target add i686-pc-windows-msvc
+if errorlevel 1 exit /b %errorlevel%
+
 cargo build --release --target i686-pc-windows-msvc
+if errorlevel 1 exit /b %errorlevel%
+
 copy target\i686-pc-windows-msvc\release\dora_runtime.lib lib\Windows\dora_runtime.lib
+if errorlevel 1 exit /b %errorlevel%
+
 msbuild ..\..\Projects\Windows\Dora.sln -p:Configuration=release
+if errorlevel 1 exit /b %errorlevel%
 
 echo Built APP in 'Projects\Windows\build\Release'
